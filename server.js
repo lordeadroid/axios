0fdb01cb79647522d1c52052b6a24d8371a81283
lordeadroid <rishabhrawat34@gmail.com>
2024-02-26 18:18:43 +0530

    local server for simulating fetch

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8000;
+
+app.get("/:id", (req, res) => {
+  const data = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+  };
+
+  const { id } = req.params;
+  res.json(data[id]);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT}`);
+});
